d0b51598b7b6da9fcc66c868efd5576029220bd9
Andre Fustes <andrefustes@gmail.com>
2025-02-02 21:29:54 -0300

    first commit

---
+++ src/main/java/org/yourcompany/yourproject/ProjetoJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Andre
+ */
+public class ProjetoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Primeiro Projeto!");
+    }
+}
